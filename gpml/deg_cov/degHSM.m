1aec07916439d6ff09dc95b5a0eb42ebaadc115e
simbartonels <simbartonels@gmail.com>
2014-09-23 18:04:22 +0200

    fixed a bunch of bugs

--- gpml/deg_cov/degHSM.m
+++ gpml/deg_cov/degHSM.m
@@ -1,14 +1,26 @@
 function K = degHSM(M, L, J, lambda, hyp, z, di)
-if nargin<5, K = '(2)'; return; end              % report number of parameters
-D = size(L, 1);
+% DEGHSM Degenerate covariance function proposed in "Hilbert Space Methods
+% for Reduced Rank Gaussian Process Regression" by Solin and Särkkä in
+% 2014. 
+% M - the number of basis functions
+% L - a vector describing the range of the inputs, i.e. x in [-L, L]
+% J - an index matrix created in initHSM.
+% lambda - the eigenvalues of the kernel corresponding to the negative 
+%   LaPlace operator
+% Hyperparameters are the same as for covSEard and in the same order.
+
+if nargin<5, K = '(D+1)'; return; end              % report number of parameters
+D = size(L, 2);
 if nargin==5
    %return weight prior
    K = getWeightPrior(lambda, M, D, hyp);
    return;
 elseif nargin==6
     sz = size(z, 1);
+    ls = exp(hyp(1:D));
+    z = z./repmat(ls'/LS(), [sz, 1]);
     K = zeros(M^D, sz);
-    xMinusAoverBMinusA = (z-repmat(L, sz, 1))./repmat(2*L, sz, 1);
+    xMinusAoverBMinusA = (z+repmat(L, sz, 1))./repmat(2*L, sz, 1);
     for k=1:M^D
         j = J(:, k)';
         K(k, :) = (prod(sqrt(1./L), 2) * prod(sin(pi * repmat(j, sz, 1) .* xMinusAoverBMinusA), 2))';
@@ -17,10 +29,9 @@ elseif nargin==7                                                        % deriva
     %error('Optimization of hyperparameters not implemented.')
     if isempty(z)
         %gradients of the weight prior
-        if di == 1
-            ls2 = exp(2*hyp(1));
-            K = getWeightPrior(lambda, M, D, hyp).*(D-ls2*lambda);
-        elseif di == 2
+        if di <= D
+            error('To do: implement!');
+        elseif di == D+1
             %the derivative is just the weight prior itself
             K = getWeightPrior(lambda, M, D, hyp);
         else
@@ -35,15 +46,18 @@ end
 end
 
 function K = getWeightPrior(lambda, M, D, hyp)
-    ls = exp(hyp(1));
-    sf = exp(hyp(2));
+    sf = exp(hyp(D+1));
     K = zeros(M^D, 1);
     for k = 1:M^D
-         K(k) = spectralDensity(lambda(k), D, ls, sf);
+         K(k) = spectralDensity(lambda(k), D, sf);
     end
 end
 
-function s = spectralDensity(rSqrd, D, ls, sf)
+function s = spectralDensity(rSqrd, D, sf)
     %see Rasmussen p.154 above (7.11)
-    s = sf*sqrt(2*pi)*(ls^D)*exp(-ls^2*rSqrd/2);
+    s = sf*sqrt(2*pi)*(LS()^D)*exp(-LS()^2*rSqrd/2);
+end
+
+function retval = LS()
+    retval = 0.1;
 end
