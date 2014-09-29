7f2f280516bfbb708fd21d6d165c926507aa5944
simbartonels <simbartonels@gmail.com>
2014-09-29 17:24:31 +0200

    improved memory footprint

--- gpml/inf/infExactDegKernel.m
+++ gpml/inf/infExactDegKernel.m
@@ -15,12 +15,17 @@ if ~strcmp(cov1,'covDegenerate'); error('Only covDegenerate supported.'), end   
 [n, D] = size(x);
 weight_prior = feval(degCov{:}, hyp.cov);
 if size(weight_prior, 2) > 1; error('Weight prior must be column vector!'); end
-SigmaInv = diag(1./weight_prior);
+%TODO: it's probably better to use repmat where possible!
+%SigmaInv = diag(1./weight_prior);
+sigmainv = 1./weight_prior;
 sn2 = exp(2*hyp.lik);                               % noise variance of likGauss
 %by convention the third argument is []. See covDegenerate.m
 Phi = feval(degCov{:}, hyp.cov, [], x);
 if size(Phi, 1) > n; error('The feature space dimensionality is greater than the number of inputs!'); end
-A = (Phi*Phi')+sn2*SigmaInv;                      % evaluate covariance matrix
+A = Phi*Phi'; %+sn2*SigmaInv;                      % evaluate covariance matrix
+M = size(Phi, 1);
+diagidx = logical(eye(M));
+A(diagidx) = A(diagidx) + sn2*sigmainv;
 L = chol(A);
 clear A;
 m = feval(mean{:}, hyp.mean, x);                          % evaluate mean vector
@@ -50,14 +55,18 @@ if nargout>1                               % do we want the marginal likelihood?
     dnlZ = hyp;
     invAPhiy = L\M;
     for i = 1:numel(hyp.cov)
-        dSigma = feval(degCov{:}, hyp.cov, [], [], i);
+        dsigma = feval(degCov{:}, hyp.cov, [], [], i);
         dPhi = feval(degCov{:}, hyp.cov, [], x, i);
-        dA = dPhi * Phi' + Phi * dPhi' - sn2 * SigmaInv * diag(dSigma) * SigmaInv; 
+        dA = dPhi * Phi' + Phi * dPhi';% - sn2 * SigmaInv * diag(dSigma) * SigmaInv;
+        dA(diagidx) = dA(diagidx) - sn2 * sigmainv .* dsigma .* sigmainv;
         dlogdetA = trace(solve_chol(L, dA)); % * detA / detA
-        dlogdetSigma = trace(SigmaInv * diag(dSigma));
+        %dlogdetSigma = trace(SigmaInv * diag(dSigma));
+        dlogdetSigma = sum(sigmainv .* dsigma);
         dMM = 2*invAPhiy'*dPhi*y - invAPhiy' * dA * invAPhiy;
         dnlZ.cov(i) = (-dMM/sn2 + dlogdetA + dlogdetSigma)/2;
     end
+    % TODO: can this be more efficient?
+    SigmaInv = diag(sigmainv);
     dnlZ.lik = -2*yyMM + ...
        2*invAPhiy'*SigmaInv*invAPhiy + ...
        trace(solve_chol(L, 2*sn2*SigmaInv)) + 2*(n-size(L, 1));
