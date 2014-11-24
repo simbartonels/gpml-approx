2ec79a47cf7c31c95e6c54969031e79f1663de7d
simbartonels <simbartonels@gmail.com>
2014-11-24 19:07:07 +0100

    extended test for SM GPR implementation in libGP

--- gpml/test/testSMlibgp.m
+++ gpml/test/testSMlibgp.m
@@ -1,18 +1,19 @@
 function testSMlibgp()
     %sd = 21988 %numerically very unstable scenario
     %sd = 8903 %llh breaks!
-    [x, y, xs, smhyp] = initEnvConcrete();
+    sd = 21371 %problems with L matrix?!
+    [x, y, ~, smhyp] = initEnvConcrete(sd);
     D = size(x, 2);
     M = (numel(smhyp.cov)-1-D)/D/2;
 
     %logIndNoise = log(0);
     %[ymu, ys2] = gp(smhyp, @infExact, [], {@covSMnaive, M, logIndNoise}, @likGauss, x, y, xs);
     %[ymuS, ys2S, ~, ~, ~, post] = gp(smhyp, @infFITC, [], {@covSM, M}, @likGauss, x, y, xs);
-    [nlZ_o, ~, post] = gp(smhyp, @infFITC, [], {@covSM, M}, @likGauss, x, y);
+    [nlZ_o, dnlZ_o, post] = gp(smhyp, @infFITC, [], {@covSM, M}, @likGauss, x, y);
     alpha_o = post.alpha;
     L_o = post.L;
     %[ymuS, ys2S, ~, ~, ~, post] = gp(smhyp, @infSMfast, [], {@covSM, M}, @likGauss, x, y, xs);
-    [nlZ, ~, post] = gp(smhyp, @infSMfast, [], {@covSM, M}, @likGauss, x, y);
+    [nlZ, dnlZ, post] = gp(smhyp, @infSMfast, [], {@covSM, M}, @likGauss, x, y);
     diff = max(max(abs(L_o - post.L)));
     if diff > 1e-10
         error('Check computation of L matrix!');
@@ -25,6 +26,12 @@ function testSMlibgp()
     if diff > 1e-10
         diff
         error('Check computation of nlZ!');
+    end
+    [diff, arg] = max(abs(unwrap(dnlZ_o)-unwrap(dnlZ)));
+    if diff > 1e-10
+        diff
+        arg
+        error('Check gradients of nlZ!');
     end
 end
 
