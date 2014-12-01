2afb899189801207a616a080fa326b0b75a7fb71
simbartonels <simbartonels@gmail.com>
2014-12-01 17:36:27 +0100

    fixed fast food libGP test

--- gpml/test/testFastFoodLibGP.m
+++ gpml/test/testFastFoodLibGP.m
@@ -1,16 +1,21 @@
-function testFastFoodlibGP()
+function testFastFoodLibGP()
     [x, y, ~, hyp] = initEnv();
+    hyp.mean = [];
     D = size(x, 2);
-    m = 4;
-    intD = nextpow2(D);
+    m = 1;
+    intD = 2^nextpow2(D);
     M = m*intD; %not *2 because we only need it for the matrices
     [alpha, L, nlZ, s, g, randpi, b] = infFastFoodmex(2*M, unwrap(hyp), x, y);
-    s = reshape(s, [M, 1]);
-    g = reshape(g, [M, 1]);
-    randpi = reshape(randpi, [M, 1]);
-    b = reshape(b, [M, 1]);
+    L = solve_chol(L, eye(size(L, 1)))*exp(2*hyp.lik);
+    s = reshape(s', [M, 1]);
+    g = reshape(g', [M, 1]);
+    randpi = reshape(randpi', [M, 1]);
+    randpi = randpi + 1;
+    b = reshape(b', [M, 1]);
     degCov = {@covDegenerate, {@degFastFood, s, g, randpi, b}};
-    [post, nlZ_o] = infExactDegKernel(hyp, [], degCov, @likGauss, x, y);
+    ff_hyp = hyp;
+    ff_hyp.cov = [hyp.cov(D+1); hyp.cov(1:D)];
+    [post, nlZ_o] = infExactDegKernel(ff_hyp, {@meanZero}, degCov, @likGauss, x, y);
     alpha_o = post.alpha;
     L_o = post.L;
     diff = max(max(abs(L_o - L)));
@@ -26,4 +31,5 @@ function testFastFoodlibGP()
         diff
         error('Check computation of nlZ!');
     end
+    disp('Test complete.');
 end
