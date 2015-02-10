65fe2bacbc4cfa3ccdf336da71b36499ddedb06c
simbartonels <simbartonels@gmail.com>
2015-02-10 18:51:48 +0100

    extended test for libGP bf implementations

--- gpml/test/libGP/testDegCovsLibGP.m
+++ gpml/test/libGP/testDegCovsLibGP.m
@@ -12,6 +12,10 @@ function testSolin()
     M = floor((n-1)^(1/D));
     [J, lambda] = initHSM(M, D, L);
     bf = {@degHSM2, M, L, J, lambda};
-    phi_o = feval(bf{:}, hyp.cov, z)
-    phi = covDegFast(bf, M, seed, hyp.cov, [], z)
+    phi_o = feval(bf{:}, hyp.cov, z);
+    phi = covDegFast(bf, seed, D^M, hyp.cov, [], z);
+    diff = max(max(abs(phi-phi_o)./abs(phi_o)))
+    if(diff > 1e-5)
+	error('Basis Function computations disagree!');
+    end
 end
