6e6c097ab66b645de8741d5c3bf2926637cecbfd
simbartonels <simbartonels@gmail.com>
2015-02-16 20:22:52 +0100

    made HSM_hyper_opt use rprop and fixed a bug in infSolinfast

--- evaluation/methods/HSM_hyper_opt.m
+++ evaluation/methods/HSM_hyper_opt.m
@@ -1,11 +1,14 @@
 function [hyp, theta_over_time, retvals] = HSM_hyper_opt(EXPERIMENT, trainX, trainY, m, D)
     %TODO: choose another L
-    %L = 1.2;
+    L = 1.2;
     infMethod = @infSolinfast;
-    %covfunc = {@covDegFast, {@degHSM2, m, L, J, lambda}, 0, m};
-    covfunc = {@covDegFast, {'degHSM2', m}, 0, D^m};
+    %covfunc = {@covDegFast, {@degHSM2, floor(m^(1/D)), L, J, lambda}, EXPERIMENT.SEED, m};
+    covfunc = {@covDegFast, {'degHSM2', floor(D^(1/m))}, EXPERIMENT.SEED, m};
     hyp.cov = [zeros(D,1);0];
-    likfunc = @likGauss; sn = 0.25; hyp.lik = log(sn);
-    [hyp, ~, ~, theta_over_time] = minimize(hyp, @gp, EXPERIMENT.NUM_HYPER_OPT_ITERATIONS, {infMethod}, [], covfunc, likfunc, trainX, trainY);
+    likfunc = @likGauss; 
+    sn = 0.25; hyp.lik = log(sn);
+    %[hyp, ~, ~, theta_over_time] = minimize(hyp, @gp, EXPERIMENT.NUM_HYPER_OPT_ITERATIONS, {infMethod}, [], covfunc, likfunc, trainX, trainY);
+    [nhyp, theta_over_time] = rpropmex(EXPERIMENT.SEED, abs(EXPERIMENT.NUM_HYPER_OPT_ITERATIONS), trainX, trainY, 'degenerate', 'CovSum (CovSEard, CovNoise)', unwrap(hyp), m, 'Solin');
+    hyp = rewrap(hyp, nhyp);
     retvals = {infMethod, covfunc, likfunc};
 end
--- gpml/inf/infSolinfast.m
+++ gpml/inf/infSolinfast.m
@@ -7,6 +7,7 @@ if ~strcmp(likstr,'likGauss')               % NOTE: no explicit call to likGauss
 end
 cov1 = cov{1}; if isa(cov1, 'function_handle'), cov1 = func2str(cov1); end
 if strcmp(cov1,'covDegFast');
+    M = cov{4};
     cov = cov{2};
 else
     error('You MUST use covDegFast to use this inference method.');
@@ -17,9 +18,6 @@ if ~(isempty(mean) || strcmp(func2str(mean{1}), 'meanZero'))
     mean
     error('Only zero-mean supported!');
 end
-%TODO: this is a hack!
-M_intern = cov{2};
-M = size(x, 2)^M_intern;
 post.sW = [];                                                  % unused for FITC
 if nargout == 1
     [alpha, L] = infSolinmex(M, unwrap(hyp), x, y);
