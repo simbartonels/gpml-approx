3d2579e24fc418ae4e8a305c742afa21cb286924
simbartonels <simbartonels@gmail.com>
2015-02-19 18:14:31 +0100

    adapted HSM to changes in EVAL_METHOD and made predict easier exchangeable (whether to use Matlab or libGP)

--- evaluation/methods/HSM_hyper_opt.m
+++ evaluation/methods/HSM_hyper_opt.m
@@ -1,16 +1,20 @@
-function [hyp, theta_over_time, retvals] = HSM_hyper_opt(EXPERIMENT, trainX, trainY, m, D)
-    %TODO: choose another L
-    L = 1.2 * ones([1, D]);
-    infMethod = @infExactDegKernel;
-    [J, lambda] = initHSM(floor(D^(1/m)), D, L);
-    covfunc = {@covDegenerate, {@degHSM2, floor(D^(1/m)), L, J, lambda}};
-    %infMethod = @infSolinfast;
-    %covfunc = {@covDegFast, {'degHSM2', floor(D^(1/m))}, EXPERIMENT.SEED, m};
+function [hyp, theta_over_time, retvals] = HSM_hyper_opt(EXPERIMENT, trainX, trainY, M, D)
+    m = floor(M^(1/D));    
     hyp.cov = [zeros(D,1);0];
     likfunc = @likGauss; 
     sn = 0.25; hyp.lik = log(sn);
-    [hyp, ~, ~, theta_over_time] = minimize(hyp, @gp, EXPERIMENT.NUM_HYPER_OPT_ITERATIONS, {infMethod}, [], covfunc, likfunc, trainX, trainY);
-    %[nhyp, theta_over_time] = rpropmex(EXPERIMENT.SEED, abs(EXPERIMENT.NUM_HYPER_OPT_ITERATIONS), trainX, trainY, 'degenerate', 'CovSum (CovSEard, CovNoise)', unwrap(hyp), m, 'Solin');
-    %hyp = rewrap(hyp, nhyp);
+    %TODO: choose another L
+    L = 1.2 * ones([1, D]);
+    [J, lambda] = initHSM(m, D, L);
+    M
+    size(lambda)
+    infMethod = @infExactDegKernel;
+    covfunc = {@covDegenerate, {@degHSM2, m, L, J, lambda}};
+    %[hyp, ~, ~, theta_over_time] = minimize(hyp, @gp, EXPERIMENT.NUM_HYPER_OPT_ITERATIONS, {infMethod}, [], covfunc, likfunc, trainX, trainY);
+    infMethod = @infSolinfast;
+    covfunc = {@covDegFast, {'degHSM2', m}, EXPERIMENT.SEED, M};
+    [nhyp, theta_over_time] = rpropmex(EXPERIMENT.SEED, abs(EXPERIMENT.NUM_HYPER_OPT_ITERATIONS), trainX, trainY, 'degenerate', 'CovSum (CovSEard, CovNoise)', unwrap(hyp), m, 'Solin');
+    hyp = rewrap(hyp, nhyp);
+    
     retvals = {infMethod, covfunc, likfunc};
 end
--- evaluation/methods/HSM_predict.m
+++ evaluation/methods/HSM_predict.m
@@ -1,3 +1,3 @@
-function [mF, s2F] = HSM_predict(EXPERIMENT, hyp, trainX, trainY, testX, retvals)
-    [mF, s2F] = gp(hyp, retvals{1}, [], retvals{2}, retvals{3}, trainX, trainY, testX);
+function [mF, s2F, nlZ] = HSM_predict(EXPERIMENT, hyp, trainX, trainY, testX, retvals)
+    [mF, s2F, ~, ~, nlZ] = gp(hyp, retvals{1}, [], retvals{2}, retvals{3}, trainX, trainY, testX);
 end
