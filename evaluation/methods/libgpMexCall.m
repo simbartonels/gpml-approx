2f4ba50f1799485909bef3a661611694a767c88e
simbartonels <simbartonels@gmail.com>
2015-03-04 18:31:10 +0100

    adapted to change in rprop interface

--- evaluation/methods/libgpMexCall.m
+++ evaluation/methods/libgpMexCall.m
@@ -1,3 +1,6 @@
 function [times, theta_over_time, mF, s2F, nlZ, mFT] = libgpMexCall(EXPERIMENT, trainX, trainY, testX, approximation, covName, initialhypers, bfname)
-	[times, theta_over_time, mF, s2F, nlZ, mFT] = rpropmex(EXPERIMENT.SEED, abs(EXPERIMENT.NUM_HYPER_OPT_ITERATIONS), trainX, trainY, testX, approximation, covName, initialhypers, EXPERIMENT.M, bfname);
+	iters = abs(EXPERIMENT.NUM_HYPER_OPT_ITERATIONS);
+	[times, theta_over_time, mF, s2F, nlZ] = rpropmex(EXPERIMENT.SEED, iters, trainX, trainY, testX, approximation, covName, initialhypers, EXPERIMENT.M, bfname);
+	%workaround
+	mFT = zeros(size(trainX, 1), iters);
 end
