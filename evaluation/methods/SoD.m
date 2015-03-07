d7adcd9e86d1b5f6db2fb17e4b75f4587dc31f56
simbartonels <simbartonels@gmail.com>
2015-03-07 15:28:58 +0100

    SoD makes use of libGP

--- evaluation/methods/SoD.m
+++ evaluation/methods/SoD.m
@@ -1,11 +1,19 @@
 function [times, theta_over_time, mF, s2F, nlZ, mFT] = SoD(EXPERIMENT, trainX, trainY, testX)
+D = size(trainX, 2);
+M = EXPERIMENT.M;
 hyp.cov = [zeros(D,1);0];
 likfunc = @likGauss; sn = 0.25; hyp.lik = log(sn);
-if strcmp(EXPERIMENT.EXTRA, 'clustering')
-	[hyp, sod, theta_over_time] = gp_sod_mine(hyp, EXPERIMENT.NUM_HYPER_OPT_ITERATIONS, {@covSEard}, likfunc, trainX, trainY, m, 'c', D, 'split');
-else
-	[hyp, sod, theta_over_time] = gp_sod_mine(hyp, EXPERIMENT.NUM_HYPER_OPT_ITERATIONS, {@covSEard}, likfunc, trainX, trainY, m, 'r');
-end
+disp('Executing FPC');
+sod = indPoints(trainX, M, 'c');
+disp('Done');
+%if strcmp(EXPERIMENT.EXTRA, 'clustering')
+%	[hyp, sod, theta_over_time] = gp_sod_mine(hyp, EXPERIMENT.NUM_HYPER_OPT_ITERATIONS, {@covSEard}, likfunc, trainX, trainY, m, 'c', D, 'split');
+%else
+%	[hyp, sod, theta_over_time] = gp_sod_mine(hyp, EXPERIMENT.NUM_HYPER_OPT_ITERATIONS, {@covSEard}, likfunc, trainX, trainY, m, 'r');
+%end
+[times, theta_over_time, mF, s2F, nlZ, ~] = libgpMexCall(EXPERIMENT, trainX(sod, :), trainY(sod, :), testX, 'full', 'CovSum (CovSEard, CovNoise)', unwrap(hyp), '');
+[~, ~, ~, mFT, ~] = infLibGPmex(trainX(sod, :), trainY(sod, :), trainX, 'full', 'CovSum (CovSEard, CovNoise)', theta_over_time(:, size(times)));
+return;
 retvals = {sod, likfunc};
 times = theta_over_time(:, 1);
 actual_iters = size(theta_over_time, 2);
