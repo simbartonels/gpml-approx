91e6f43683d7bad4931cbbfc7d9cfc1e67537ce4
simbartonels <simbartonels@gmail.com>
2015-03-09 17:46:28 +0100

    improved output

--- gpml/test/util/checkError.m
+++ gpml/test/util/checkError.m
@@ -5,6 +5,8 @@ if any(any(isinf(v1))), error('%s has Infs in %s!', m1, s); end
 if any(any(isnan(v2))), error('%s has Infs in %s!', m2, s); end
 diff = max(max(abs((v1 - v2)./v1)));
 if diff > 1e-5
+    v1
+    v2
     error('%s and %s implementation disagree in %s.', m1, m2, s);
 end
 end
