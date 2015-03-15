d26eed80376bb71d7117fa959de2cc4b797ae80d
simbartonels <simbartonels@gmail.com>
2015-03-15 13:45:14 +0100

    fixed bugs in RERUN script

--- evaluation/RERUN_HYPERPARAMETER_CONFIGURATION.m
+++ evaluation/RERUN_HYPERPARAMETER_CONFIGURATION.m
@@ -1,12 +1,12 @@
-datasets = 'PUMADYN';
+datasets = 'CT_SLICES';
 EXPERIMENT.DATASET_FOLD = 4
 approximation = 'degenerate';
 %bfname = 'SparseMultiScaleGP';
 bfname = 'FastFood';
 EXPERIMENT.METHOD = 'FastFood';
 EXPERIMENT.M = 2048;
-m = 20;
-
+m = 2;
+trial = 1;
 
 % Choose the approximation to use: SoD, Local, FITC.
 % To use a custom method, create a test[method_name].m script 
@@ -25,10 +25,6 @@ addpath(genpath('../figtree-0.9.3/matlab'));
 maxNumCompThreads(1);
 startup
 EXPERIMENT.DATASET = datasets;
-EXPERIMENT.DATASET_FOLDS = 1; %total number of folds will be determined in load_data
-% For SoD, FITC and Local EXPERIMENT.M is a list of the 
-% subset sizes, inducing point set sizes and cluster sizes resp.
-%EXPERIMENT.M = [32 64 128 256 512 1024 2048]; 
 
 % Save raw experiment results here.
 EXPERIMENT.RESULTS_DIR = './results/'; 
@@ -39,24 +35,25 @@ EXPERIMENT.RESULTS_DIR = './results/';
 
 loadData;
 EXPERIMENT.DATASET_FOLDS = 1;
-%if the data set is evaluated using cross validation we do that here
-loadData;
-EVAL_METHOD;
-varTest=var(testY);
+varTest=var(testY)
 meanTest=mean(testY);
 varTrain=var(trainY);
 meanTrain=mean(trainY);
-load(sprintf('%sresults%s_%s_fold%s_M%d', RESULTS_DIR, EXPERIMENT.METHOD, EXPERIMENT.DATASET, EXPERIMENT.DATASET_FOLD, EXPERIMENT.M));
+constant_average_predictor = mse(meanTrain, testY, meanTest, varTest)
+load(sprintf('%sresults%s_%s_fold%d_M%d', EXPERIMENT.RESULTS_DIR, EXPERIMENT.METHOD, EXPERIMENT.DATASET, EXPERIMENT.DATASET_FOLD, EXPERIMENT.M));
 results = eval(sprintf('results%s', EXPERIMENT.METHOD));
+theta_over_time = results.hyp_over_time{trial};
 %EXPERIMENT.SEED = results.seeds(m);
 seed = 0; %TODO: use upper line
+rng('default');
 rng(seed);
 covName = 'CovSum (CovSEard, CovNoise)';
-[~, ~, ~, mFT, ~] = infLibGPmex(trainX, trainY, trainX, approximation, covName, theta_over_time(:, m), EXPERIMENT.M, bfname);
+%[~, ~, ~, mFT, ~] = infLibGPmex(trainX, trainY, trainX, approximation, covName, theta_over_time(:, m), EXPERIMENT.M, bfname);
 [~, ~, ~, mF, ~] = infLibGPmex(trainX, trainY, testX, approximation, covName, theta_over_time(:, m), EXPERIMENT.M, bfname);
 
 disp('Training error: ');
-last_train_error = mse(mFT, trainY, meanTrain, varTrain)
+%last_train_error = mse(mFT, trainY, meanTrain, varTrain)
 last_test_error = mse(mF, testY, meanTest, varTest)
+test_rms_error = sqrt(mean((mF-testY).^2))
 disp('NaNs or Infs: ');
 any(isnan(mFT) | isinf(abs(mFT)))
