f6d068238660766b75387a443d5e760a4b7e1799
simbartonels <simbartonels@gmail.com>
2015-03-27 01:01:57 +0100

    sod saves chosen subset now

--- evaluation/methods/SoD.m
+++ evaluation/methods/SoD.m
@@ -5,6 +5,7 @@ hyp.cov = [zeros(D,1);0];
 likfunc = @likGauss; sn = 0.25; hyp.lik = log(sn);
 disp('Executing FPC');
 sod = indPoints(trainX, M, 'c');
+EXPERIMENT.SOD{trial_id} = sod;
 disp('Done');
 [times, theta_over_time, mF, s2F, nlZ, ~] = libgpMexCall(EXPERIMENT, trainX(sod, :), trainY(sod, :), testX, 'full', 'CovSum (CovSEard, CovNoise)', unwrap(hyp), '');
 [~, ~, ~, mFT, ~] = infLibGPmex(trainX(sod, :), trainY(sod, :), trainX, 'full', 'CovSum (CovSEard, CovNoise)', theta_over_time(:, size(times)));
