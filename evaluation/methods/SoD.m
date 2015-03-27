b427e9bc41cf71540730993427018bb01189ac78
simbartonels <simbartonels@gmail.com>
2015-03-27 13:11:02 +0100

    SoD loads previous points if possible

--- evaluation/methods/SoD.m
+++ evaluation/methods/SoD.m
@@ -1,11 +1,18 @@
 function [times, theta_over_time, mF, s2F, nlZ, mFT] = SoD(EXPERIMENT, trainX, trainY, testX, trial_id)
+error('Make sure to test this method first!');
 D = size(trainX, 2);
 M = EXPERIMENT.M;
 hyp.cov = [zeros(D,1);0];
 likfunc = @likGauss; sn = 0.25; hyp.lik = log(sn);
 disp('Executing FPC');
 sod = indPoints(trainX, M, 'c');
-EXPERIMENT.SOD{trial_id} = sod;
+if ~exist('EXPERIMENT.SOD') 
+	EXPERIMENT.SOD{trial_id} = sod;
+elseif size(EXPERIMENT.SOD, 2) < trial_id
+	EXPERIMENT.SOD{trial_id} = sod;
+else
+	sod = EXPERIMENT.SOD{trial_id};
+end
 disp('Done');
 [times, theta_over_time, mF, s2F, nlZ, ~] = libgpMexCall(EXPERIMENT, trainX(sod, :), trainY(sod, :), testX, 'full', 'CovSum (CovSEard, CovNoise)', unwrap(hyp), '');
 [~, ~, ~, mFT, ~] = infLibGPmex(trainX(sod, :), trainY(sod, :), trainX, 'full', 'CovSum (CovSEard, CovNoise)', theta_over_time(:, size(times)));
