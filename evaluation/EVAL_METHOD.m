25fe027da1336a5398f52e618191a3977ecee899
simbartonels <simbartonels@gmail.com>
2015-03-27 13:09:40 +0100

    changed eval_method to produce clenaer result files libgp can now start from previous hyper parameters

--- evaluation/EVAL_METHOD.m
+++ evaluation/EVAL_METHOD.m
@@ -1,37 +1,64 @@
+error('Make sure to test this method first.');
 global testTime;
 varTest=var(testY);
 meanTest=mean(testY);
 varTrain=var(trainY);
 meanTrain=mean(trainY);
 
-len = abs(EXPERIMENT.NUM_HYPER_OPT_ITERATIONS);
+iters = EXPERIMENT.NUM_HYPER_OPT_ITERATIONS;
+len = abs(iters);
+num_trials = EXPERIMENT.NUM_TRIALS
+for trial_id = 1:num_trials
+	seed = floor(rand(1) * 32000);
+	EXPERIMENT.SEED{trial_id} = seed;
+end
+
 %check if there already some results
 
 resultVarName = sprintf('results%s', EXPERIMENT.METHOD);
 results_file = sprintf('%s%s%s%s%s_fold%d_M%d.mat', EXPERIMENT.RESULTS_DIR, '', EXPERIMENT.DATASET, filesep, resultVarName, EXPERIMENT.DATASET_FOLD, EXPERIMENT.M)
-error('');
 if exist(results_file, 'file') == 2
 	disp('WARNING: An experiment results file already exists for this configuration. Attempting continuation of the experiment.');
 	load(results_file);
 	resultsOut = eval(resultVarName);
         [trials_old, len_old] = size(resultOut.('hyp_time'));
-	if len_old ~= len
-		error('The current number of hyper-parameter optimization steps (%d) is different from the previous number of steps (%d). This is not allowed yet.', len, len_old);
-	else
-	        first_trial_id = trials_old + 1;
+	disp('The following Experiment description was loaded and is used...');
+	EXPERIMENT = resultsOut.EXPERIMENT;
+	EXPERIMENT.NUM_HYPER_OPT_ITERATIONS = iters;
+	EXPERIMENT.NUM_TRIALS = num_trials;
+%	if len_old ~= len
+%		error('The current number of hyper-parameter optimization steps (%d) is different from the previous number of steps (%d). This is not allowed yet.', len, len_old);
+%	else
+	if len_old >= abs(iters)
+		first_trial_id = trials_old + 1;
 	end
+	for trial_id = trials_old + 1:EXPERIMENT.NUM_TRIALS
+		seed = floor(rand(1) * 32000);
+		EXPERIMENT.SEED{trial_id} = seed;
+	end
+%	end
 else
-	resultOut.('msll') = zeros(EXPERIMENT.NUM_TRIALS, len);
-	resultOut.('mse') = zeros(EXPERIMENT.NUM_TRIALS, len);;
-	resultOut.('llh') = zeros(EXPERIMENT.NUM_TRIALS, len);;
-	resultOut.('tmse') = zeros(EXPERIMENT.NUM_TRIALS, len);;
+	len_old = 0;
+	%resultOut.('msll') = zeros(EXPERIMENT.NUM_TRIALS, len);
+	%resultOut.('mse') = zeros(EXPERIMENT.NUM_TRIALS, len);;
+	%resultOut.('llh') = zeros(EXPERIMENT.NUM_TRIALS, len);;
+	%resultOut.('tmse') = zeros(EXPERIMENT.NUM_TRIALS, len);;
 
-	resultOut.('hyp_time') = zeros(EXPERIMENT.NUM_TRIALS, len);;
-	resultOut.('train_time') = zeros(EXPERIMENT.NUM_TRIALS, len);;
-	resultOut.('test_time') = zeros(EXPERIMENT.NUM_TRIALS, len);;
+	%resultOut.('hyp_time') = -ones(EXPERIMENT.NUM_TRIALS, len);;
+	%resultOut.('train_time') = zeros(EXPERIMENT.NUM_TRIALS, len);;
+	%resultOut.('test_time') = zeros(EXPERIMENT.NUM_TRIALS, len);;
+	
+	resultOut.('msll') = zeros(1, 1);
+	resultOut.('mse') = zeros(1, 1);;
+	resultOut.('llh') = zeros(1, 1);;
+	resultOut.('tmse') = zeros(1, 1);;
+
+	resultOut.('hyp_time') = -ones(1, 1);;
+	resultOut.('train_time') = zeros(1, 1);;
+	resultOut.('test_time') = zeros(1, 1);;
 	resultOut.N_train = length(trainY);
 	resultOut.N_test = length(testY);
-	resultOut.('seeds') = zeros(EXPERIMENT.NUM_TRIALS, 1);
+	%resultOut.('seeds') = zeros(EXPERIMENT.NUM_TRIALS, 1);
 	resultOut.('hyp_over_time') = {};
         first_trial_id = 1;
 
@@ -41,41 +68,48 @@ end
 if first_trial_id <= EXPERIMENT.NUM_TRIALS
     m = EXPERIMENT.M;
     for trial_id = first_trial_id:EXPERIMENT.NUM_TRIALS
-	seed = floor(rand(1) * 32000);
-	EXPERIMENT.SEED = seed;
+	if trial_id > size(resultOut.hyp_time, 1)
+		len_old = 0;
+	else
+		EXPERIMENT.LAST_HYPERS{trial_id} = resoutOut.hyp_over_time{triald_id}(:, end); 
+	end
+	seed = EXPERIMENT.SEED{trial_id};
 	rng('default');
 	rng(seed);
-    	resultsOut.('seeds')(trial_id) = seed;
+    	resultsOut.('seeds'){trial_id} = seed;
 
         %----------------------------------------
         % Optimize hyperparameters.
         %----------------------------------------
-        [times, theta_over_time, mF, s2F, nlZ, mFT] = feval(EXPERIMENT.METHOD, EXPERIMENT, trainX, trainY, testX, trial_id);
+	EXPERIMENT.LAST_TRIAL = trial_id;
+	EXPERIMENT.NUM_HYPER_OPT_ITERATIONS = len - len_old;
+        [times, theta_over_time, mF, s2F, nlZ, ~] = feval(EXPERIMENT.METHOD, EXPERIMENT, trainX, trainY, testX, trial_id);
+	EXPERIMENT.NUM_HYPER_OPT_ITERATIONS = iters;
         %----------------------------------------
         % Save data.
         %----------------------------------------
-        resultOut.('hyp_time')(trial_id, :) = times;
         
         % I just assignt the same prediction time
-        resultOut.('train_time')(trial_id, :) = 0; %predTime-testTime;
-        resultOut.('test_time')(trial_id, :) = 0; %testTime;
+        %resultOut.('train_time')(trial_id, :) = 0; %predTime-testTime;
+        %resultOut.('test_time')(trial_id, :) = 0; %testTime;
         %resultOut.('hyps'){trial_id} = [resultOut.('hyps'){trial_id} theta_over_time]; %[resultOut.('hyps'){trial_id} rewrap(theta_over_time];
-        resultOut.('hyp_over_time'){trial_id} = theta_over_time;
         for i=1:size(times)
             if times(i) < 0, break, end
-            disp(sprintf('Calculating MSE for iteration %d', i)); 
+	    resultOut.('hyp_time')(trial_id, i) = times(i);
+	    resultOut.('hyp_over_time'){trial_id}(:, i) = theta_over_time(:, i);
+            %disp(sprintf('Calculating MSE for iteration %d', i)); 
             resultOut.('msll')(trial_id, i) = mnlp(mF(:, i),testY,s2F(:, i), meanTest, varTest);
             resultOut.('mse')(trial_id, i)  = mse(mF(:, i),testY, meanTest, varTest);
             %resultOut.('tmse')(trial_id, i)  = mse(mFT(:, i),trainY, meanTrain, varTrain);
+	    resultOut.('llh')(trial_id, i) = nlZ(i);    
         end
-        disp('Training error: ');
-        if size(mFT, 2) > 1, mFT = mFT(:, size(times, 1)); end
-        last_train_error = mse(mFT, trainY, meanTrain, varTrain)
+        disp('Last test error: ');
+        %if size(mFT, 2) > 1, mFT = mFT(:, size(times, 1)); end
+        %last_train_error = mse(mFT, trainY, meanTrain, varTrain)
         last_test_error = mse(mF(:, size(times, 1)), testY, meanTest, varTest)
         disp('NaNs or Infs: ');
         any(isnan(mFT) | isinf(abs(mFT)))
 
-        resultOut.('llh')(trial_id, :) = nlZ;    
 	resultOut.('EXPERIMENT') = EXPERIMENT;
         eval(sprintf('%s=resultOut;', resultVarName));
         save(results_file, resultVarName);
--- evaluation/methods/libgpMexCall.m
+++ evaluation/methods/libgpMexCall.m
@@ -1,8 +1,12 @@
 function [times, theta_over_time, mF, s2F, nlZ, mFT] = libgpMexCall(EXPERIMENT, trainX, trainY, testX, approximation, covName, initialhypers, bfname)
+	current_trial = EXPERIMENT.LAST_TRIAL;
+	if exist('EXPERIMENT.LAST_HYPERS')
+		if size(EXPERIMENT.LAST_HYPERS, 2) >= current_trial, initialhypers = EXPERIMENT.LAST_HYPERS{current_trial}; end
+	end
 	iters = abs(EXPERIMENT.NUM_HYPER_OPT_ITERATIONS);
-	[times, theta_over_time, mF, s2F, nlZ] = rpropmex(EXPERIMENT.SEED, iters, trainX, trainY, testX, approximation, covName, initialhypers, EXPERIMENT.M, bfname);
+	[times, theta_over_time, mF, s2F, nlZ] = rpropmex(EXPERIMENT.SEED{current_trial}, iters, trainX, trainY, testX, approximation, covName, initialhypers, EXPERIMENT.M, bfname);
 	%workaround
-	%mFT = zeros(size(trainX, 1), iters);    
-    [~, ~, ~, mFT, ~] = infLibGPmex(trainX, trainY, trainX, approximation, covName, theta_over_time(:, size(times)), EXPERIMENT.M, bfname);
+	mFT = zeros(size(trainX, 1), iters);    
+    %[~, ~, ~, mFT, ~] = infLibGPmex(trainX, trainY, trainX, approximation, covName, theta_over_time(:, size(times)), EXPERIMENT.M, bfname);
     %[~, ~, ~, mFT, ~] = infLibGPmex(trainX, trainY, testX, approximation, covName, theta_over_time(:, size(times)), EXPERIMENT.M, bfname);
 end
