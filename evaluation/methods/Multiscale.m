7b6c0107b9ccb458e487a4a95a65dddcc938a2fa
simbartonels <simbartonels@gmail.com>
2015-04-06 14:15:21 +0200

    Multiscale makes again use of FIC

--- evaluation/methods/Multiscale.m
+++ evaluation/methods/Multiscale.m
@@ -2,35 +2,45 @@ function [EXPERIMENT, times, theta_over_time, mF, s2F, nlZ, gradNorms, mFT] = Mu
     D = size(trainX, 2);
     M = EXPERIMENT.M;
     sn = 0.25; hyp.lik = log(sn);
-    disp('Executing FPC');
-    %[~, U] = indPoints(trainX, M, 'c');
-    %U = U';
-    sod = indPoints(trainX, M, 'c');
-    U = trainX(sod, :);
-    disp('Done.');
+%    disp('Executing FPC');
+%    %[~, U] = indPoints(trainX, M, 'c');
+%    %U = U';
+%    sod = indPoints(trainX, M, 'c');
+%    U = trainX(sod, :);
+%    disp('Done.');
 %    size(U)
-    U = reshape(U, [M*D, 1]);
-    ell = zeros(D,1);
-    ellU = log(ones([M*D, 1])/2);
-    sf2 = 0;
-    hyp = [ell; ellU; U; sf2; log(sn)];
-    [times, theta_over_time, mF, s2F, nlZ, gradNorms, mFT] = libgpMexCall(EXPERIMENT, trainX, trainY, testX, 'OptMultiscale', 'CovSum (CovSEard, CovNoise)', hyp, 'SparseMultiScaleGP');
-return;
+%    U = reshape(U, [M*D, 1]);
+%    ell = zeros(D,1);
+%    ellU = log(ones([M*D, 1])/2);
+%    sf2 = 0;
+%    hyp = [ell; ellU; U; sf2; log(sn)];
+%    [times, theta_over_time, mF, s2F, nlZ, gradNorms, mFT] = libgpMexCall(EXPERIMENT, trainX, trainY, testX, 'OptMultiscale', 'CovSum (CovSEard, CovNoise)', hyp, 'SparseMultiScaleGP');
+%return;
 
-%    filename = sprintf('%s%s%sresultsFIC_fold%d_M%d.mat', ...
-%	EXPERIMENT.RESULTS_DIR, EXPERIMENT.DATASET, filesep, EXPERIMENT.DATASET_FOLD, EXPERIMENT.M)
-%    resultsFIC = load(filename);
-%    resultsFIC = resultsFIC.resultsFIC;
+    filename = sprintf('%s%s%sresultsFIC_fold%d_M%d.mat', ...
+	EXPERIMENT.RESULTS_DIR, EXPERIMENT.DATASET, filesep, EXPERIMENT.DATASET_FOLD, EXPERIMENT.M)
+    resultsFIC = load(filename);
+    resultsFIC = resultsFIC.resultsFIC;
     
     FIChyp = resultsFIC.hyp_over_time{trial_id};
-    ind = size(FIChyp, 2)
+    ind = floor(size(FIChyp, 2)/2);
 %    ind = 25 %TODO: remove
     FIChyp = FIChyp(:, ind);
-    time_offset = resultsFIC.hyp_time(trial_id, ind);
+    time_offset = resultsFIC.hyp_time{trial_id}(ind);
 %    time_offset = time_offset(ind);
     disp('Setting noise to 0 for FIC and Multiscale compliance.');
-    FIChyp(size(FIChyp, 1)) = -Inf;
+    FIChyp(size(FIChyp, 1)) = 1e-50;
     hyp = FIC_params_to_Multiscale(FIChyp, D, M);
-    [times, theta_over_time, mF, s2F, nlZ, mFT] = libgpMexCall(EXPERIMENT, trainX, trainY, testX, 'OptMultiscale', 'CovSum (CovSEard, CovNoise)', hyp, 'SparseMultiScaleGP');
+    cap = EXPERIMENT.CAP_TIME;
+	%are we continuing an experiment? ...
+    	if isfield(EXPERIMENT, 'LAST_HYPERS')
+		if size(EXPERIMENT.LAST_HYPERS, 2) >= trial_id
+			% in that case the captime is taken care of in libgpMexCall
+			time_offset = 0;
+		end
+	end
+    EXPERIMENT.CAP_TIME = cap - time_offset;
+    [times, theta_over_time, mF, s2F, nlZ, gradNorms, mFT] = libgpMexCall(EXPERIMENT, trainX, trainY, testX, 'OptMultiscale', 'CovSum (CovSEard, CovNoise)', hyp, 'SparseMultiScaleGP');
     times = times + time_offset;
+    EXPERIMENT.CAP_TIME = cap;
 end
