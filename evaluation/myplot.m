abeed1df04c89985aa3967e2fce19ffcac4d2739
simbartonels <simbartonels@gmail.com>
2015-04-07 13:14:51 +0200

    plotscript bugfix

--- evaluation/myplot.m
+++ evaluation/myplot.m
@@ -1,14 +1,25 @@
 time_mean_cells = {};
 value_mean_cells = {};
 for j=1:size(x_value, 2)
-    plot(x_value{j}, y_value{j}, 'x', 'Color', plot_colors{method_id});
-    for k=1:size(x_value{j}, 2)
-        if size(time_mean_cells, 2) >= k
-            time_mean_cells{k} = [time_mean_cells{k}, x_value{j}(k)];
-            value_mean_cells{k} = [value_mean_cells{k}, y_value{j}(k)];
-        else 
-            time_mean_cells{k} = x_value{j}(k);
-            value_mean_cells{k} = y_value{j}(k);
+    plot(x_value{j}, y_value{j}, '.', 'Color', plot_colors{method_id});
+    for k=1:max(size(x_value{j}, 2), size(time_mean_cells, 2))
+        make_copy_from_last_cell = true;
+        if k <= size(x_value{j}, 2)
+            if size(time_mean_cells, 2) >= k
+                time_mean_cells{k} = [time_mean_cells{k}, x_value{j}(k)];
+                value_mean_cells{k} = [value_mean_cells{k}, y_value{j}(k)];
+            else
+                time_mean_cells{k} = x_value{j}(k);
+                
+                if k > 1 
+                    previous_cell = value_mean_cells{k-1}(1:end-1);
+                else
+                    previous_cell = [];
+                end
+                value_mean_cells{k} = [previous_cell, y_value{j}(k)];
+            end
+        else
+            value_mean_cells{k} = [value_mean_cells{k}, y_value{j}(end)];
         end
     end
 end
