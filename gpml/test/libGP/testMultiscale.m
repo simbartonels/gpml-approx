8cc47b40106f41eb8416f7f10f00d76dfcce2ab9
simbartonels <simbartonels@gmail.com>
2015-04-07 23:36:30 +0200

    extended multiscale test

--- gpml/test/libGP/testMultiscale.m
+++ gpml/test/libGP/testMultiscale.m
@@ -1,4 +1,37 @@
 function testMultiscale()
+	testMSvsFICbf();
+	testMultiscalevsFIC();
+
+end
+
+function testMSvsFICbf()
+	M = 20;
+        D = 12;
+	z = randn([2, D]);
+	fichyp = randn([D+1+M*D+1, 1]);
+	uvx = bfmex('FIC', 0, M, fichyp, D, z);
+	hyp = FIC_params_to_Multiscale(fichyp, D, M);
+    f = sum(fichyp(1:D)) %/2;
+    lsf = 2*fichyp(D+1)+f;    
+    hyp(end-1) = lsf;
+    exp(2*fichyp(D+1))
+	uvxSM = bfmex('SparseMultiScaleGP', 0, M, hyp, D, z);
+	checkError(uvx, exp(lsf)*uvxSM, 'FIC', 'MS', 'phi');
+
+        Kuu = bfmex('FIC', 0, M, fichyp, D);
+	L = chol(Kuu);
+	KuuSM = bfmex('SparseMultiScaleGP', 0, M, hyp, D);
+	LSM = chol(KuuSM);
+	checkError(L, exp(lsf)*LSM, 'FIC', 'MS', 'chol(Kuu)');
+	a = L\uvx;
+	b = LSM\uvxSM;
+	checkError(a, b, 'FIC', 'MS', 'chol(Kuu)^-1 phi');
+	disp('Basis Function test successful.');
+
+end
+
+
+function testMultiscalevsFIC()
 %tests on a real dataset if FIC and Multiscale agree.
 me = mfilename;                                            % what is my filename
 mydir = which(me); mydir = mydir(1:end-2-numel(me));
@@ -37,14 +70,57 @@ meanTrain=mean(trainY);
 
     M = 20;
     ind = 25;
-    FIChyp = resultsFIC.hyp_over_time{1};
-    FIChyp(size(FIChyp, 1), ind) = -Inf;
+    FIChyp = resultsFIC.hyp_over_time{1}(: , ind);
+    %FIChyp(size(FIChyp, 1), ind) = -Inf;
     disp('Setting noise to 0 for FIC and Multiscale compliance.');
-    [~, ~, ~, mFfic, ~] = infLibGPmex(trainX, trainY, testX, 'FIC', 'CovSum (CovSEard, CovNoise)', FIChyp(: , ind), M, 'FIC');
+    [~, L, ~, mFfic, ~] = infLibGPmex(trainX, trainY, testX, 'FIC', 'CovSum (CovSEard, CovNoise)', FIChyp, M, 'FIC');
+diag(L)'
 FICtestError = mean((mFfic-testY).^2./varTest)
-    hyp = FIC_params_to_Multiscale(FIChyp(:, ind), D, M);
-    [~, ~, ~, mF, ~] = infLibGPmex(trainX, trainY, testX, 'FIC', 'CovSum (CovSEard, CovNoise)', hyp, M, 'SparseMultiScaleGP');
+    hyp = FIC_params_to_Multiscale(FIChyp, D, M);
+    f = sum(FIChyp(1:D)) %/2;
+    lsf = 2*FIChyp(D+1)+f;    
+    hyp(end-1) = lsf;
+    hyp(end-1)
+
+	%uvx = bfmex('FIC', 0, M, FIChyp, D, trainX);
+        %Kuu = bfmex('FIC', 0, M, FIChyp, D);
+	%L = chol(Kuu);
+	%a = L\uvx;
+	%a(1:5, 1:5)
+	%uvxSM = bfmex('SparseMultiScaleGP', 0, M, hyp, D, trainX);
+	%KuuSM = bfmex('SparseMultiScaleGP', 0, M, hyp, D);
+	%LSM = chol(KuuSM);
+	%b = LSM\uvxSM;
+	%b(1:5, 1:5)
+%return;
+    [~, L, ~, mF, ~] = infLibGPmex(trainX, trainY, testX, 'FIC', 'CovSum (CovSEard, CovNoise)', hyp, M, 'SparseMultiScaleGP');
+diag(L)'
 testError = mean((mF-testY).^2./varTest)
+
+prediction_diff = mean(abs((mFfic - mF)./mFfic))
+%if prediction_diff > 1e-5, error('FIC and Multiscale predictions should agree in this setting!'); end
+M = 50;
+    filename = [mydir 'resultsFIC_fold1_M50.mat'];
+    resultsFIC = load(filename);
+    resultsFIC = resultsFIC.resultsFIC;
+    ind = size(resultsFIC.hyp_over_time{1}, 2)/2;
+    FIChyp = resultsFIC.hyp_over_time{1}(:, ind);
+    %FIChyp(end) = -10;
+    %FIChyp = log(abs(randn(size(FIChyp)))/2);
+    %FIChyp = zeros(size(FIChyp))-abs(randn(size(FIChyp)));
+    disp('Setting noise to 0 for FIC and Multiscale compliance.');
+    [~, L, ~, mFfic, ~] = infLibGPmex(trainX, trainY, testX, 'FIC', 'CovSum (CovSEard, CovNoise)', FIChyp, M, 'FIC');
+FICtestError = mean((mFfic-testY).^2./varTest)
+    hyp = FIC_params_to_Multiscale(FIChyp, D, M);
+    %hyp(end-1) = FIChyp(D+1);
+    diag(L)'
+    ind_noise_factor = 1e-6;%*sqrt(prod(exp(logell))*(2*pi)^D);
+    [alpha, L, ~, mF, ~] = infLibGPmex(trainX, trainY, testX, 'FIC', 'CovSum (CovSEard, CovNoise)', hyp, M, 'SparseMultiScaleGP', 0, ind_noise_factor);
+diag(L)'
+exp(hyp(end-1))
+prod(2*exp(hyp(1:D)))
+testError = mean((mF-testY).^2./varTest)
+
 
 prediction_diff = mean(abs((mFfic - mF)./mFfic))
 if prediction_diff > 1e-5, error('FIC and Multiscale predictions should agree in this setting!'); end
