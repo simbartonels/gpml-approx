52695ba542de19747bca6f6ee318f2079e399b59
simbartonels <simbartonels@gmail.com>
2015-04-08 21:51:09 +0200

    fixed how parameters are changed for libGP (where 2*pi is not considered)

--- gpml/util/FIC_params_to_Multiscale.m
+++ gpml/util/FIC_params_to_Multiscale.m
@@ -1,11 +1,13 @@
 function smhyp = FIC_params_to_Multiscale(hyp, D, M, V)
+    two_pi = 2 * pi;
     if nargin < 4
 	V = hyp((D+1+1):(D+1+D*M));
+        two_pi = 1; %libgp mode
     end
-    if hyp(size(hyp, 1)) > -Inf, disp('WARNING: FIC and Multiscale differ when noise is greater 0.'); end
+    %if hyp(size(hyp, 1)) > -Inf, disp('WARNING: FIC and Multiscale differ when noise is greater 0.'); end
     logell = 2 * hyp(1:D);
-    f = (log(2*pi)*D+sum(logell))/2;
-    f = log(sqrt(prod(exp(logell))*(2*pi)^D));
+    f = (log(two_pi)*D+sum(logell))/2;
+    %f = log(sqrt(prod(exp(logell))*(two_pi)^D));
     lsf = 2 * hyp(D+1)+f;
     logsigma = repmat(logell'-log(2), M, 1);
     smhyp = [logell; reshape(logsigma, [M*D, 1]); ...
