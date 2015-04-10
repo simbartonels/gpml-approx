108181c48e5e2b86580a0cadf01c6c6b144041b0
simbartonels <simbartonels@gmail.com>
2015-04-10 02:06:00 +0200

    changed back to sampling in libgp

--- evaluation/methods/FastFood.m
+++ evaluation/methods/FastFood.m
@@ -2,10 +2,11 @@ function [EXPERIMENT, times, theta_over_time, mF, s2F, nlZ, gradNorms, mFT] = Fa
     D = size(trainX, 2);
     hyp.cov = [zeros(D,1);0];
     sn = 0.25; hyp.lik = log(sn);
-    m = floor(EXPERIMENT.M/2/D);
-    [s, g, ~, b] = initFastFood(m, D, []);
-    Dintern = 2^nextpow2(D);
-    shape = [Dintern, m];
-    EXPERIMENT.EXTRA = [reshape(s, shape); reshape(g, shape); reshape(b, shape)];
+   % m = floor(EXPERIMENT.M/2/D);
+   % [s, g, ~, b] = initFastFood(m, D, []);
+   % Dintern = 2^nextpow2(D);
+   % shape = [Dintern, m];
+   % EXPERIMENT.EXTRA = [reshape(s, shape); reshape(g, shape); reshape(b, shape)];
+   % EXPERIMENT = rmfield(EXPERIMENT, 'EXTRA'); %otherwise we'd overwrite our random matrices with whatever's in there ...
     [times, theta_over_time, mF, s2F, nlZ, gradNorms, mFT] = libgpMexCall(EXPERIMENT, trainX, trainY, testX, 'degenerate', 'CovSum (CovSEard, CovNoise)', unwrap(hyp), 'FastFood');
 end
