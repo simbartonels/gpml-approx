b77cfd5b846e7a027bd35037c7646616f611718e
simbartonels <simbartonels@gmail.com>
2015-04-10 02:04:31 +0200

    analyze results computes and stores the overall prediction time for the test set to check consistency

--- evaluation/ANALYZE_RESULTS.m
+++ evaluation/ANALYZE_RESULTS.m
@@ -23,29 +23,80 @@
 % Krzysztof Chalupka, University of Edinburgh 
 % and California Institute of Technology, 2012
 % kjchalup@caltech.edu
-
+clear;
 me = mfilename;                                            % what is my filename
 mydir = which(me); mydir = mydir(1:end-2-numel(me));        % where am I located
 RESULTS_DIR = [mydir, 'results', filesep];
 PLOTS_DIR = [mydir, 'plots', filesep]; % Ready plots go here.
 DATASETS = {'PRECIPITATION'} % Plot data for these datasets only.
-METHODS = {'HSM', 'FastFood', 'SoD', 'FullSE'}%, 'FICfixed'} % Plot data for these methods only.
-Ms = {2048, 2048, 1940, 1500} %, 1000}
-
+METHODS = {'HSM', 'FastFood', 'SoD', 'FIC'}%, 'FICfixed'} % Plot data for these methods only.
+Ms = {2048, 2048, 1940, 1500, 1500} %, 1000}
+%DATASETS = {'PUMADYN'}
+%METHODS = {'FIC', 'FastFood', 'SoD'}%, 'Multiscale'}
+%Ms = {75, 198, 85, 75}
+%METHODS = {'FIC', 'Multiscale', 'MultiscaleFIC'};
+%Ms = {50, 50, 25}
+%DATASETS = {'CPU'}
+%METHODS = {'MultiscaleFIC', 'FIC'}%, 'FastFood', 'FIC'}
+%Ms = {20, 20} 
 plot_colors = {'r', 'g', 'b', 'k', 'c'}; % At least as many colors as methods 
                                     % plotted. 
 PLOTFILETYPE='pdf';
 FILENAME_SUFFIX = '_unedited';
 
-fold = '4'
+fold = '1'
 
 figure, close
 for dset_id = 1:length(DATASETS)
     dataset = DATASETS{dset_id};
+    dataset_loaded = false;
     for method_id = 1:length(METHODS)
         method = METHODS{method_id};
         % Load data.
-        load(sprintf('%s%s%sresults%s_fold%s_M%d', RESULTS_DIR, dataset, filesep, method, fold, Ms{method_id}));        
+	results_file = sprintf('%s%s%sresults%s_fold%s_M%d', RESULTS_DIR, dataset, filesep, method, fold, Ms{method_id});
+        load(results_file);
+	method = METHODS{method_id};
+	resultVarName = sprintf('results%s', method);
+        results = eval(resultVarName);
+	if ~isfield(results, 'pred_time') & ~strcmp(method, 'FullSE')
+		EXPERIMENT = results.EXPERIMENT;
+		if ~dataset_loaded
+			EXPERIMENT.PREPROCESS_DATASET = true;
+			loadData;
+			dataset_loaded = true;
+		end
+		if strcmp(method, 'FastFood')
+			gpname = 'degenerate';
+			bfname = 'FastFood';
+			rmfield(EXPERIMENT, 'EXTRA');
+		elseif strcmp(method, 'HSM')
+			gpname = 'degenerate';
+			bfname = 'FastFood';
+		elseif strcmp(method, 'SoD')
+			gpname = 'full';
+			bfname = '';
+			trainX = trainX(EXPERIMENT.SOD{1}, :);
+			trainY = trainY(EXPERIMENT.SOD{1}, :);
+		elseif strcmp(method, 'FIC')
+			gpname = 'OptFIC';
+			bfname = 'FIC';
+		elseif strcmp(method, 'Multiscale')
+			gpname = 'OptMultiscale';
+			bfname = 'SparseMultiScaleGP';
+		else
+			error('Unkown method name: %s', method);
+		end
+		if isfield(EXPERIMENT, 'EXTRA')
+	                [~, ~, ~, ~, ~, t] = infLibGPmex(trainX, trainY, testX, gpname, 'CovSum (CovSEard, CovNoise)', results.hyp_over_time{1}(:, 1), EXPERIMENT.M, bfname, EXPERIMENT.SEED{1}, EXPERIMENT.EXTRA)
+		else
+			[~, ~, ~, ~, ~, t] = infLibGPmex(trainX, trainY, testX, gpname, 'CovSum (CovSEard, CovNoise)', results.hyp_over_time{1}(:, 1), EXPERIMENT.M, bfname, EXPERIMENT.SEED{1})
+		end
+		results.pred_time = t;
+		%avg_pred_time = t / size(testX, 1)
+		eval(sprintf('%s=results;', resultVarName));
+        	%save(results_file, resultVarName);
+	end
+	prediction_time = results.pred_time
     end
 end
 for dset_id = 1:length(DATASETS)
