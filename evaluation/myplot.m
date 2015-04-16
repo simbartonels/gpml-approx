85ff022167b7000cf3b0b69ff9b8d9a2580db7f8
simbartonels <simbartonels@gmail.com>
2015-04-17 00:47:05 +0200

    improved plots

--- evaluation/myplot.m
+++ evaluation/myplot.m
@@ -1,7 +1,7 @@
 time_mean_cells = {};
 value_mean_cells = {};
 for j=1:size(x_value, 2)
-    plot(x_value{j}, y_value{j}, '.', 'Color', plot_colors{method_id});
+    plot(x_value{j}, y_value{j}, '.', 'Color', plot_colors{method_id}, 'markersize', 3);
     if results.grad_norms{j}(end) < 1e-4
 	%converged
 	last_step_symbol = '+';
@@ -12,27 +12,31 @@ for j=1:size(x_value, 2)
 	%timeout
 	last_step_symbol = '*';
     end
-   plot(x_value{j}(end), y_value{j}(end), last_step_symbol, 'Color', plot_colors{method_id});
+    plot(x_value{j}(end), y_value{j}(end), last_step_symbol, 'Color', plot_colors{method_id}, 'markersize', 10);
     % plotting mean values
-    for k=1:max(size(x_value{j}, 2), size(time_mean_cells, 2))
-        make_copy_from_last_cell = true;
+    initial_k = 2;
+    if strcmp(method, 'Multiscale'), initial_k = 1; end
+    for k=initial_k:max(size(x_value{j}, 2), size(time_mean_cells, 2))
         if k <= size(x_value{j}, 2)
-            if size(time_mean_cells, 2) >= k
+            if k <= size(time_mean_cells, 2)
                 time_mean_cells{k} = [time_mean_cells{k}, x_value{j}(k)];
                 value_mean_cells{k} = [value_mean_cells{k}, y_value{j}(k)];
             else
-                time_mean_cells{k} = x_value{j}(k);
-                
-                if k > 1 
+%                 time_mean_cells{k} = x_value{j}(k);
+                if k > initial_k
                     previous_cell = value_mean_cells{k-1}(1:end-1);
+                    previous_cell2 = time_mean_cells{k-1}(1:end-1);
                 else
                     previous_cell = [];
+                    previous_cell2 = [];
                 end
                 value_mean_cells{k} = [previous_cell, y_value{j}(k)];
+                time_mean_cells{k} = [previous_cell2, x_value{j}(k)];
             end
         else
             value_mean_cells{k} = [value_mean_cells{k}, y_value{j}(end)];
+            time_mean_cells{k} = [time_mean_cells{k}, x_value{j}(end)];
         end
     end
 end
-plots{method_id} = plot(cellfun(@mean, time_mean_cells), cellfun(@mean, value_mean_cells), '-', 'Color', plot_colors{method_id});
+plots{method_id} = plot(cellfun(@mean, time_mean_cells), cellfun(@mean, value_mean_cells), '-', 'Color', plot_colors{method_id}, 'linewidth', 1);
